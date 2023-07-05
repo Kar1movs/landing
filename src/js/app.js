e1eacd26a6a0bb68488c7938d3430b79168400c6
Suhrob <abdiyazovs@mail.ru>
2023-07-05 16:07:33 +0500

    first main

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+
+import "/src/libs/js/bootstrap.js"
+
+flsFunctions.isWebp();
+
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+const swiper = new Swiper()
